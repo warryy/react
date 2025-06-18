a690f5fecafff89d26c4f35135b4c1012635ba7c
warryy <warryy@qq.com>
2025-06-18 17:15:44 +0800

    feat: react

---
+++ webpack/v18/src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>Hello, React + TSX!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<App />);
--- webpack/v18/webpack.config.js
+++ webpack/v18/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./src/index.ts",
+  entry: "./src/index.tsx",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
